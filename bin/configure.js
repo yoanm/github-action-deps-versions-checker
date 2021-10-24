d6b552b2ecb1c1eb17feecbbbcddb9b11712379f
Yoanm <yoanm@users.noreply.github.com>
2021-10-24 08:33:44 +0200

    DEBUG

--- bin/configure.js
+++ bin/configure.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-require-imports,@typescript-eslint/no-var-requires
 const { execSync } = require("child_process");
 execSync('echo "Yarn version: `yarn --version`"', { stdio: "inherit" });
-execSync(`yarn --cwd "${__dirname}/../" install --prod  --non-interactive --no-progress`, { stdio: "inherit" });
-execSync(`yarn --cwd "${__dirname}/../" list --depth=0 --prod --non-interactive --no-progress`, { stdio: "inherit" });
+const projectRootDir = `${__dirname}/../`;
+execSync(`yarn install --prod  --non-interactive --no-progress`, { stdio: "inherit", cwd: projectRootDir });
+execSync(`yarn list --depth=0 --prod --non-interactive --no-progress`, { stdio: "inherit", cwd: projectRootDir });
